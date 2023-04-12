f68fb65c46920bf90f9cab2f75f70f414f760415
Feeenix <feeeeeeenix@gmail.com>
2023-04-12 21:46:58 +0200

    masse funksjoner i lib.js

--- javascript/lib.js
+++ javascript/lib.js
@@ -20,3 +20,38 @@ async function readFiles(filesArray) { // tar inn et array med fil-objekter og r
     }
     return imageDataUrls;
 }
+
+
+// disse to funksjonene brukes for å omgjøre localStorage til en string, som vi kan dele med andre folk som så kan importere den til sin egen localStorage
+function exportLocalStorage() { // exporterer localStorage til en string
+    let data = JSON.stringify((localStorage));
+    console.log(data.replace(/"/g, "\\\"")); // bruker regex til å erstatte alle " med \"
+}
+function importLocalStorage(jsonDataString) { // importerer localStorage fra en string
+    // jsonDataString = ""
+    localStorage.clear();
+    let jsonData = JSON.parse(jsonDataString);
+    for (let key in jsonData) {
+        localStorage.setItem(key, jsonData[key]);
+    }
+}
+
+
+
+
+function hentFraLocalStorage(key) { // returnerer et objekt fra localStorage
+    if (localStorage.getItem(key) === null) {
+        return {};
+    }
+    let data = localStorage.getItem(key);
+    return JSON.parse(data);
+}
+
+
+function brukernavnErLedig(brukernavn) {
+    let usernames = Object.keys(hentFraLocalStorage("users"));
+    if (usernames.includes(brukernavn)) {
+        return false;
+    }
+    return true;
+}

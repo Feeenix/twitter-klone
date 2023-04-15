d7c32b7d7c544b06be569c136eddce6a296c39ed
Feeenix <feeeeeeenix@gmail.com>
2023-04-15 18:34:13 +0200

    aaa

--- javascript/redirectHvisLoggetUt.js
+++ javascript/redirectHvisLoggetUt.js
@@ -1,10 +1,15 @@
 window.addEventListener("load", (function () {
-    let [brukernavn, utlopstid] = JSON.parse(hentFraLocalStorage("loggedInUser"));
+    console.log(hentFraLocalStorage("loggedInUser"))
+    let loggedInUser = hentFraLocalStorage("loggedInUser");
+    let brukernavn = loggedInUser["brukernavn"] 
+    let utlopstid = loggedInUser["utlopstid"]
     if (!brukernavnFinnes(brukernavn)){
         loggUtBruker();
+        console.log("brukernavn finnes ikke")
     }
     if (Date.now() > utlopstid) {
         loggUtBruker();
+        console.log("utlopstid er passert")
     }
     
     

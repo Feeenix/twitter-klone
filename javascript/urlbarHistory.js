fd7e3fa5e4660666b9f7e66d0a67d14a3c7937f6
Feeenix <feeeeeeenix@gmail.com>
2023-04-23 20:53:49 +0200

    back button

--- javascript/urlbarHistory.js
+++ javascript/urlbarHistory.js
@@ -1,9 +1,39 @@
-let history = localStorage.getItem("history");
+function leggTilHistory() {
+    let history = JSON.parse(localStorage.getItem("history"));
 
-if (history == null) {
-    history = [];
+    if (history == null) {
+        history = [];
+        console.log("history er null")
+    }
+    let currentUrl = window.location.href;
+
+    history.push(currentUrl);
+    console.log(typeof history)
+    localStorage.setItem("history", JSON.stringify(history));
 }
-let currentUrl = window.location.href;
+function popHistory() {
+    let history = JSON.parse(localStorage.getItem("history"));
+    let sisteURL = history.pop();
+    localStorage.setItem("history", JSON.stringify(history));
+    return sisteURL;
+}
+function clearHistory() {
+    localStorage.setItem("history", "[]");
+}
 
-history.push(currentUrl);
-localStorage.setItem("history", history);
+function goBack() {
+    popHistory();
+    let previousPage = popHistory();
+    if (previousPage == undefined) {
+        previousPage = "home.html";
+    }
+    window.location.href = previousPage;
+}
+
+let backButtons = document.querySelectorAll(".backButton");
+for (let i = 0; i < backButtons.length; i++) {
+    backButtons[i].addEventListener("click", goBack);
+}
+
+
+
